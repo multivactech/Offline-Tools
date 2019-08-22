4c7d76486dbfc609bf0d2ff100eed16cab31c8c6
DavisLinger <1095661456@qq.com>
2019-08-22 16:05:22 +0800

    fix:助记词需求
    修正错误处理的方式
    增加依赖包到vendor

--- mnemonic/mnemonic.go
+++ mnemonic/mnemonic.go
@@ -9,15 +9,15 @@ import (
 	"golang.org/x/crypto/ed25519"
 )
 
+// Account is the account data including public- and private- key and the corresponding mnemonic.
 type Account struct {
 	PrivateKey string
 	PublicKey  string
 	Mnemonic   string
-	Err        error
 }
 
 // GenerateMnemonicByLength generate mnemonic and then use mnemonic to generate the private key and its public key
-func GenerateMnemonicByLength(length int) *Account {
+func GenerateMnemonicByLength(length int) (*Account, error) {
 	// Length of mnemonic must be in the key list in mneMap.If length is error，there will no mnemonic.
 	mneMap := map[int]int{
 		12: 128,
@@ -31,8 +31,7 @@ func GenerateMnemonicByLength(length int) *Account {
 			PublicKey:  "",
 			PrivateKey: "",
 			Mnemonic:   "",
-			Err:        fmt.Errorf("非法长度,长度必须为（其中一个）:12，15，18，21，24"),
-		}
+		}, fmt.Errorf("非法长度,长度必须为（其中一个）:12，15，18，21，24")
 	}
 	bitSize := mneMap[length]
 	entropy, err := bip39.NewEntropy(bitSize)
@@ -41,8 +40,7 @@ func GenerateMnemonicByLength(length int) *Account {
 			PublicKey:  "",
 			PrivateKey: "",
 			Mnemonic:   "",
-			Err:        fmt.Errorf("生成随机序列失败"),
-		}
+		}, fmt.Errorf("生成随机序列失败,err:%v", err)
 	}
 	mnemonic, err := bip39.NewMnemonic(entropy)
 	if err != nil {
@@ -50,8 +48,7 @@ func GenerateMnemonicByLength(length int) *Account {
 			PublicKey:  "",
 			PrivateKey: "",
 			Mnemonic:   "",
-			Err:        fmt.Errorf("助记词字典加载错误"),
-		}
+		}, fmt.Errorf("助记词字典加载错误,err:%v", err)
 	}
 	// Default that there is no password.
 	seed := bip39.NewSeed(mnemonic, "")
@@ -63,15 +60,13 @@ func GenerateMnemonicByLength(length int) *Account {
 			PrivateKey: "",
 			PublicKey:  "",
 			Mnemonic:   "",
-			Err:        fmt.Errorf("密钥生成失败，err:%v", err),
-		}
+		}, fmt.Errorf("密钥生成失败，err:%v", err)
 	}
 	return &Account{
 		PrivateKey: hex.EncodeToString(prv),
 		PublicKey:  hex.EncodeToString(pub),
 		Mnemonic:   mnemonic,
-		Err:        nil,
-	}
+	}, nil
 }
 
 // MnemonicToPrivateKey get private key and public key by using mnemonic.Returns publickey,privatekey,error
