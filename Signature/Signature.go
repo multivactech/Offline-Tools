0f30042916bc333aa2e0af50047529aa824c7390
gtygo <gtydoit@gmail.com>
2019-12-25 12:11:34 +0800

    feat : add sign info for offline tools (#7)
    *  a
    
    * del testcase

--- Signature/Signature.go
+++ Signature/Signature.go
@@ -2,12 +2,48 @@ package signature
 
 import (
 	"encoding/hex"
+	"encoding/json"
 	"fmt"
+	"github.com/multivactech/Offline-Tools/Account"
 	"golang.org/x/crypto/ed25519"
 )
 
 // PrivateKey defines the data structure of MultiVAC project private key.
 type PrivateKey ed25519.PrivateKey
+
+type SignInfo struct {
+	TxHex     string `json:"txhex"`
+	SignData  string `json:"signdata"`
+	PublicKey []byte `json:"pubkey"`
+}
+
+//handle sign info through `SignInfo` struct hex decode.
+func HandleSignInfo(unSignedInfoJSONHex string, privKey string) (string, error) {
+	pubKey, err := Account.PrivatekeyToPublickey(privKey)
+	if err != nil {
+		return "", err
+	}
+	signInfoBytes, err := hex.DecodeString(unSignedInfoJSONHex)
+	if err != nil {
+		return "", err
+	}
+	var signInfoJson SignInfo
+
+	if err = json.Unmarshal(signInfoBytes, &signInfoJson); err != nil {
+		return "", err
+	}
+	signData, err := Sign(privKey, signInfoJson.SignData)
+	signInfoJson.SignData = string(signData)
+	signInfoJson.PublicKey = pubKey
+
+	signedJsonBytes, err := json.Marshal(signInfoJson)
+	if err != nil {
+		return "", err
+	}
+	signedInfoJSONHex := hex.EncodeToString(signedJsonBytes)
+
+	return signedInfoJSONHex, nil
+}
 
 // Sign the data with a private key.
 func Sign(privateKey string, message string) ([]byte, error) {
