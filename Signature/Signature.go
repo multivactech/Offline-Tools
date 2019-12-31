2943d2bcc9d0009fd202b6ef468ec326e522129b
gtygo <gtydoit@gmail.com>
2019-12-31 16:04:36 +0800

    refactor : view to cli

--- Signature/Signature.go
+++ Signature/Signature.go
@@ -2,9 +2,7 @@ package signature
 
 import (
 	"encoding/hex"
-	"encoding/json"
 	"fmt"
-	"github.com/multivactech/Offline-Tools/Account"
 	"golang.org/x/crypto/ed25519"
 )
 
@@ -17,32 +15,7 @@ type SignInfo struct {
 }
 
 //handle sign info through `SignInfo` struct hex decode.
-func HandleSignInfo(unSignedInfoJSONHex string, privKey string) (string, error) {
-	pubKey, err := Account.PrivatekeyToPublickey(privKey)
-	if err != nil {
-		return "", err
-	}
-	signInfoBytes, err := hex.DecodeString(unSignedInfoJSONHex)
-	if err != nil {
-		return "", err
-	}
-	var signInfoJson SignInfo
 
-	if err = json.Unmarshal(signInfoBytes, &signInfoJson); err != nil {
-		return "", err
-	}
-	signData, err := Sign(privKey, signInfoJson.SignData)
-	signInfoJson.SignData = string(signData)
-	signInfoJson.PublicKey = pubKey
-
-	signedJsonBytes, err := json.Marshal(signInfoJson)
-	if err != nil {
-		return "", err
-	}
-	signedInfoJSONHex := hex.EncodeToString(signedJsonBytes)
-
-	return signedInfoJSONHex, nil
-}
 
 // Sign the data with a private key.
 func Sign(privateKey string, message []byte) ([]byte, error) {
